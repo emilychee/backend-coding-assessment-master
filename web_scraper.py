9f76e2716eceb0f6b2802d1ab3c0c541fec6e637
Emily Chatterjee <echatter@umich.edu>
2019-10-23 20:16:03 -0400

    scraping all relevant variables

--- web_scraper.py
+++ web_scraper.py
@@ -3,6 +3,27 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
+def find_rating(rating_element):
+	# put all classes into dictionary
+	classes = {}
+	for element in rating_element["class"]:
+		classes[element] = 1
+
+	# check which rating it is
+	if "rating-50" in classes:
+		return 5
+	elif "rating-40" in classes:
+		return 4
+	elif "rating-30" in classes:
+		return 3
+	elif "rating-20" in classes:
+		return 2
+	elif "rating-10" in classes:
+		return 1
+	else:
+		return -1
+
+
 def main():
 	reviews = {}
 	top_users = {}
@@ -19,11 +40,12 @@ def main():
 	soup = BeautifulSoup(page1.text, "html.parser")
 	for review in soup.findAll('div', attrs={'class': 'review-entry'}):
 		user = review.find('span', attrs={'class': 'italic'}).text.split()[1]
-		rating = review.find('div', attrs={'class': 'rating-static'}).text
+		rating_element = review.find('div', attrs={'class': 'rating-static'})
+		rating = find_rating(rating_element)
 		content = review.find('p', attrs={'class': 'review-content'}).text
 		recommend = review.find('div', attrs={'class': 'boldest'}).text.strip()
 		
-		print(recommend)
+		print(rating)
 		if user not in reviews:
 			reviews[user] = {"review": content, "rating": rating, "recommend": recommend}
 	
